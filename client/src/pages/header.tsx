c39931eb66a35849a733db78c0828daa449df758
Felipe d'Almeida <felipe_dalmeida@hotmail.com>
2023-05-09 22:36:09 -0300

    minor changes

--- client/src/pages/header.tsx
+++ client/src/pages/header.tsx
@@ -2,6 +2,7 @@ import Nav from "../components/Nav"
 import { Link } from 'react-router-dom';
 import Button from "../components/Button";
 import { useGlobalStore } from "../service/useGlobalStore";
+import { AuthToken } from "../service/authToken";
 
 
 
@@ -9,13 +10,19 @@ const Header = () => {
 
     const setUser = useGlobalStore((state) => state.setUser);
     const logOut=()=>{
-        
-        setUser({  isAuthenticated: false });
+
+        AuthToken.remove()
+        setUser({
+              isAuthenticated: false,
+              id:0,
+              email:"",
+              nome:"" 
+            });
+            
     }
 
 
     const anchor = [<Link to='/cadastro'>{"Cadastar Paciente"}</Link>, <Link to='/pacientes'>{"Pacientes"}</Link>]
-    //const anchor2 = [<Button className={"mt-6 md:mt-1"} title={"Logout"} onClick={() => setIsAuth(false)} />,<div className={" self-center"}>{user}</div>]
     const anchor2 = [<Link to='/usuario'>{"Usuario"}</Link>,<Button className={"mt-6 md:mt-1"} title={"Logout"} onClick={() => logOut()} />]
 
     return <>
